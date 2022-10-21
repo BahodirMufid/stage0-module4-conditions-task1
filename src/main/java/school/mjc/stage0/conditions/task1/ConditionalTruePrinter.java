ef92683beb6aff90432a0a6e8975312c0f08d0d4
bahod <bahodirmufid@gmail.com>
2022-10-21 14:37:09 +0500

    all files is up to date

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int i=0 , w=1;
+        if (i<w){
+            System.out.println(true);
+        }else {
+            System.out.println(true);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+
+        int i =0;
+
+        if (i>=0){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
     }
 }
